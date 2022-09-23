97f4e7ecff69a4666a0e246ee8789cbed8e48294
Prakhar <forestlaw319@gmail.com>
2022-09-23 22:12:58 +0530

    Removed pics app

--- muvi/muvi/urls.py
+++ muvi/muvi/urls.py
@@ -5,7 +5,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pics/', include('pics.urls')),
     path('vids/', include('vids.urls'))
 ]
 if settings.DEBUG:
